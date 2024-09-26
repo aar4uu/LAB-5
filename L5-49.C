0001c20d658727779dd3bfe7597c55456b1234ff
aar4uu <aarjupawara1125@gmail.com>
2024-09-26 14:50:46 +0530

    Add files via upload

---
+++ L5-49.C
@@ -0,0 +1,20 @@
+//PATTERN 3- Program 49
+#include<stdio.h>
+int main()
+{
+    int rows; 
+    printf("Enter the value of rows: "); 
+    scanf("%d",&rows);
+    
+    for(int i =1; i <=rows; i++)
+    {
+        for(int j =1; j <=i; j++)
+        {
+            
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
